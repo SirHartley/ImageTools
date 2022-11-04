6d63ebb2cf2aea83fa030555e0f729e7ec3ea2b9
Philipp Arbeit <philipp.hartl@zetaworks.at>
2022-11-04 10:25:51 +0100

    Rename utility

--- src/Renamer.java
+++ src/Renamer.java
@@ -7,14 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Renamer {
-    public static String folderURL = "C:\\Users\\philipp\\Desktop\\DataAnalytics\\graphics\\illustrated_entities\\png";
+    public static String folderURL = "C:\\Users\\philipp\\Desktop\\DataAnalytics\\graphics\\illustrated_entities\\images";
 
     public static void main(String[] args) throws IOException {
         int num = 0;
 
         final File folder = new File(folderURL);
 
-        for (final File fileEntry : folder.listFiles()) {
+        for (File fileEntry : folder.listFiles()) {
             num++;
             System.out.println("Image: " + num);
             fileEntry.renameTo(new File(folderURL +"\\"+ num + ".png"));
